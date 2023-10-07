90c36fed6a7ed7351af458c5889cc2f16375104c
soujatyaBB <soujatya.tech2016@outlook.com>
2023-10-08 01:42:14 +0530

    sixth commit

--- code.py
+++ code.py
@@ -1,10 +1,6 @@
 a,b = input("Enter Two Numbers : ").split()
 print("Result : ",int(a) + int(b))
 print("Result : ",int(a) - int(b))
-<<<<<<< HEAD
 print("Result : ",int(a) * int(b))
 print("Result : ",int(a) // int(b))
-=======
->>>>>>> parent of 385b2ea... Third Commit
-
 
